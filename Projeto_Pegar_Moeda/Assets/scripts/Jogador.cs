6c59fa3d5e9d83a31f2af73572692bcc8d440c75
Odailton Marinho <odailton_marinho1000@hotmail.com>
2014-11-21 21:40:09 -0200

    aaa

---
+++ Projeto_Pegar_Moeda/Assets/scripts/Jogador.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jogador : MonoBehaviour {
+
+	GUIStyle gui = new GUIStyle ();
+	int moedas = 0;
+	void Start () {
+		Debug.Log (moedas.ToString());
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider c)
+	{
+		if (c.tag == "Moeda") 
+		{
+			moedas++;
+			Debug.Log (moedas);
+		}
+		
+		
+	}
+
+	void OnGUI()
+	{
+		GUI.contentColor = Color.white;
+		gui.fontSize = 26;
+		gui.fontStyle = FontStyle.Bold;
+		gui.normal.textColor = Color.white;
+
+		GUI.TextArea(new Rect(10, 10, 200, 30), "");
+		GUI.Label (new Rect (13, 10, 200, 50), "Moedas: " + moedas, gui);
+	}
+}
---
+++ Projeto_Pegar_Moeda/Assets/scripts/RotaçãoMoeda.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotaçãoMoeda : MonoBehaviour {
+	
+	
+	public AudioClip a;
+	int incremento_z = 3;
+	void Start () {
+		//gameObject.particleSystem.enableEmission = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int z = 3;
+		transform.Rotate (0, 0, z);
+		z += incremento_z;
+	
+	}
+
+	void OnTriggerEnter(Collider c)
+	{
+		if (c.tag == "Player") 
+		{
+			incremento_z = 12;
+			audio.clip = a;
+			audio.Play(44100);
+			yield return new WaitForSeconds(audio.clip.length);
+			Destroy (gameObject);
+
+	    }
+
+
+	}
+
+}
