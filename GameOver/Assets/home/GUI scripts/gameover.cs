d694e5ed72a711ef73101205bbe3339fe4413689
brunojamelle <brunogeek9@gmail.com>
2014-11-22 14:05:00 -0300

    tela de game over

---
+++ GameOver/Assets/home/GUI scripts/gameover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameover : MonoBehaviour {
+	void OnGUI(){
+		GUILayout.BeginArea (new Rect (700, 350, 200, 400));
+		GUILayout.BeginVertical ();
+		//GUI.Box (new Rect (0,0,400,50), "");
+		if (GUILayout.Button ("Tentar Novamente")) {
+			Application.LoadLevel("");
+			Debug.Log("jogandu");
+		}
+		if (GUILayout.Button ("Sair")) {
+			Application.Quit ();
+		}
+		
+		GUILayout.EndVertical ();
+		GUILayout.EndArea ();    
+		
+
+	}
+}
+
+
