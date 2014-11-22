3fae24b427c0d3bc99fc92eb197d00ca915a9ee3
brunojamelle <brunogeek9@gmail.com>
2014-11-22 00:31:16 -0300

    menu versao 1

---
+++ menu/Assets/home/GUI scripts/menu.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menu : MonoBehaviour {
+	public bool showOptions = false;
+	public bool showCredits = false;
+	private int guiButtonWidth = 100;
+	// Use this for initialization
+
+	/*
+		var Count = 30;
+
+		function Update()
+		{
+		if (BotaoAcao.comecou == true) {
+		Count = Count - Time.time;
+		print(Count);
+		} 
+		}
+	 */
+	//public Texture t;
+	void OnGUI(){
+		/*GUI.Box (new Rect(300,350,Screen.width/2 -130,Screen.height/2 -130),"");
+		GUI.Button (new Rect (100, 100,Screen.width / 2 - 100, Screen.height / 2 - 100), "Start Game");
+		*/
+		GUILayout.BeginArea (new Rect (0, 0, 200, 300));
+		GUILayout.BeginVertical ();
+		if (GUILayout.Button ("Start Game")) {
+			Application.LoadLevel ("");
+			Debug.Log("Clicked the image");
+		}
+		if (GUILayout.Button ("Options")) {
+			showOptions = true;
+		}
+		if (GUILayout.Button ("Credits")) {
+			showCredits = true;
+		}
+		if (GUILayout.Button ("Quit Game")) {
+			Application.Quit ();
+		}
+		
+		GUILayout.EndVertical ();
+		GUILayout.EndArea ();    
+		
+		if (showOptions) {
+			showOptions = true;
+			
+			GUILayout.BeginArea (new Rect (210, 0, 200, 300));
+			GUILayout.BeginHorizontal ();
+			//text column
+			GUILayout.Label ("Quality settings", GUILayout.Width (100));
+			GUILayout.EndHorizontal ();
+			//settings column
+			GUILayout.BeginHorizontal ();
+			GUILayout.Button ("Fastest");
+			GUILayout.Button ("Fast");
+			GUILayout.Button ("Simple");
+			GUILayout.Button ("Good");
+			GUILayout.Button ("Beautiful");
+			GUILayout.Button ("Fantastic");
+			GUILayout.EndHorizontal ();
+			GUILayout.EndArea ();
+		} else {
+			showOptions = false;
+		}
+		if (showCredits) {
+			showCredits = true;
+			GUILayout.BeginArea (new Rect (210, 0, 200, 300));
+			GUILayout.BeginHorizontal ();
+			GUILayout.Button ("You did it");
+			GUILayout.EndHorizontal ();
+			GUILayout.EndArea ();
+		} else {
+			showCredits = false;
+		}
+	}
+
+
+
+}
