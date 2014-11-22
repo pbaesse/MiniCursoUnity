a02241dfb0a805f04ba147092bb1c3fa7fd7b5b9
brunojamelle <brunogeek9@gmail.com>
2014-11-22 16:42:31 -0300

    tela do fim do nivel um,n coloquei a imagem q thyago mandou pq ela tinha uma qualidade muito baixa e com isso ficaria meio q com serilhado quando colocase no plane

---
+++ Fimdenivel/Assets/home/GUI scripts/fimdelevel.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fimdelevel : MonoBehaviour {
+	void OnGUI(){
+
+		GUILayout.BeginArea (new Rect (200, 200, 200, 200));
+		GUILayout.BeginHorizontal ();
+		GUILayout.Label ("Seu tempo foi: "+" segundos");
+		GUILayout.EndHorizontal ();
+		GUILayout.EndArea ();
+
+		GUILayout.BeginArea (new Rect (1000, 500, 200, 400));
+		GUILayout.BeginHorizontal ();
+		if (GUILayout.Button ("Nivel 2")) {
+			Application.LoadLevel("");
+			Debug.Log("jogandu");
+		}
+		if (GUILayout.Button ("Repetir Nivel")) {
+			Application.LoadLevel("");
+			Debug.Log("jogandu2");
+		}
+		if (GUILayout.Button ("sair")) {
+			Application.Quit();
+		}
+		
+		GUILayout.EndHorizontal ();
+		GUILayout.EndArea ();    
+		
+
+	}
+}
