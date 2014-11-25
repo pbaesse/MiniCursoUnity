3bb79aab2014218aac1c88f5afe58b0b781af4a2
Odailton Marinho <odailton_marinho1000@hotmail.com>
2014-11-25 00:12:20 -0200

    taí, desgraça

---
+++ Projeto_com_zumbi/Assets/scripts/GUI scripts/fimdelevel.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fimdelevel : MonoBehaviour {
+	static Contador cont = new Contador();
+	float min = Mathf.Round(Contador.Minutos);
+	float seg = Mathf.Round(Contador.Segundos);
+
+	void OnGUI(){
+			GameObject.Find("Tempo").guiText.text = "Seu tempo foi:  0" + min.ToString() + ": 0" + seg.ToString();
+		GUILayout.BeginArea (new Rect (1000, 500, 200, 400));
+		GUILayout.BeginHorizontal ();
+		if (GUILayout.Button ("Nivel 2")) {
+			Application.LoadLevel("level2");
+			Debug.Log("jogandu");
+		}
+		if (GUILayout.Button ("Level 1")) {
+			Application.LoadLevel("Level1");
+			Debug.Log("jogandu2");
+		}
+		if (GUILayout.Button ("sair")) {
+			Application.Quit();
+		}
+		
+		GUILayout.EndHorizontal ();
+		GUILayout.EndArea ();    
+		
+
+	}
+}
---
+++ Projeto_com_zumbi/Assets/scripts/GUI scripts/gameover.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameover : MonoBehaviour {
+	string level = controlador_de_level.get_ultimoLevel();
+	void OnGUI(){
+		GUILayout.BeginArea (new Rect (700, 350, 200, 400));
+		GUILayout.BeginVertical ();
+		//GUI.Box (new Rect (0,0,400,50), "");
+
+		if (GUILayout.Button ("Tentar Novamente")) {
+			if(level== "level1" ){
+				Application.LoadLevel(level);
+			}
+			if(level == "level2"){
+
+				Application.LoadLevel(level);	
+
+			}
+		}
+		
+		if (GUILayout.Button ("Menu")) {
+			Application.LoadLevel("Menu");
+		}
+		if (GUILayout.Button ("Sair")) {
+			Application.Quit();
+		}
+		GUILayout.EndVertical ();
+		GUILayout.EndArea ();    
+		
+
+	}
+}
+
+
